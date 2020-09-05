62b9ffacb8ff16f85179951fea23705d3498c1ca
Hoang Luu <hoang.luu8@mavs.uta.edu>
2020-09-05 11:05:12 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a Test website from <code>Hoang Luu</code>
         </p>
         <a
           className="App-link"
