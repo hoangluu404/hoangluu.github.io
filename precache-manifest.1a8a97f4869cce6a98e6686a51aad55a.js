5f08f471ad5fe8629df5e7d574d8b9b08018770a
Hoang Luu <hoang.luu8@mavs.uta.edu>
2020-09-05 10:56:23 -0500

    Updates

---
+++ precache-manifest.1a8a97f4869cce6a98e6686a51aad55a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e618e79c39b0c6db4bc098784910ae",
+    "url": "/hoangluu.github.io/index.html"
+  },
+  {
+    "revision": "0df2f3bb6e2973d72bfb",
+    "url": "/hoangluu.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "32f389f0d7683a63a4f1",
+    "url": "/hoangluu.github.io/static/js/2.7858a370.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hoangluu.github.io/static/js/2.7858a370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df2f3bb6e2973d72bfb",
+    "url": "/hoangluu.github.io/static/js/main.f6e133e8.chunk.js"
+  },
+  {
+    "revision": "5de44dfa075874210151",
+    "url": "/hoangluu.github.io/static/js/runtime-main.c53b3fbf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hoangluu.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
